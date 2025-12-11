5ca3dd7e446bf491b951596a7861d77cac86e44d
Nguyễn  Đức Anh <nguyenducanh05032003@gmail.com>
2025-12-11 10:49:02 +0700

    feat: Fix port for deployment

--- run.py
+++ run.py
@@ -19,5 +19,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
